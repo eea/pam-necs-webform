07ba3bed44e9dda890662c68c6cdb943d2362db3
Mauro Michielon <mauro.michielon@eea.europa.eu>
2019-01-16 13:56:15 +0100

    first commit

---
+++ webform/manifest.config.js
@@ -0,0 +1,32 @@
+//basic configuration settings used to create the webform-project-export.metadata-file
+
+module.exports = {
+  "production" : {
+    "title" : "National Emission Ceiling Directive (NECD)",
+    "file" : {
+      "name" : "index.html"
+    },
+    "newXmlFileName" : "pam-nec.xml",
+    "emptyInstanceUrl" : "https://webforms.eionet.europa.eu/download/project/mmr-necs/file/instance-empty.xml",
+    "description" : "National Emission Ceiling Directive (NECD) web questionnaire",
+    "xmlSchema" : "http://dd.eionet.europa.eu/schemas/mmr-necs/MMR_NECS.xsd",
+    "active" : true,
+    "localForm" : true,
+    "remoteForm" : true,
+    "fileType" : "WEBFORM"
+  },
+  "development" : {
+    "title" : "National Emission Ceiling Directive (NECD)",
+    "file" : {
+      "name" : "index.html"
+    },
+    "newXmlFileName" : "pam-nec.xml",
+    "emptyInstanceUrl" : "https://webq2test.eionet.europa.eu/download/project/mmr-necs/file/instance-empty.xml",
+    "description" : "National Emission Ceiling Directive (NECD) web questionnaire",
+    "xmlSchema" : "https://webq2test.eionet.europa.eu/download/project/mmr-necs/file/PAM_NECS.xsd",
+    "active" : true,
+    "localForm" : true,
+    "remoteForm" : true,
+    "fileType" : "WEBFORM"
+  }
+}
