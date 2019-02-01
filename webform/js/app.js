0541613addfd9ff51b2482c43e734af8bc9783e9
Mauro Michielon <mauro.michielon@eea.europa.eu>
2019-02-01 18:41:12 +0100

    xmlns -> xsi

--- webform/js/app.js
+++ webform/js/app.js
@@ -687,8 +687,8 @@ webform.controller('NECsCtrl', ['$scope', '$rootScope', '$location', 'dataReposi
             if (!instance.NEC_PAMs['@xmlns:xsi']) $scope.instance.NEC_PAMs['@xmlns:xsi'] = 'http://www.w3.org/2001/XMLSchema-instance';                                   
             else $scope.instance.NEC_PAMs['@xmlns:xsi'] = instance.NEC_PAMs['@xmlns:xsi'];
 
-            if (!instance.NEC_PAMs['@xmlns:noNamespaceSchemaLocation']) $scope.instance.NEC_PAMs['@xmlns:noNamespaceSchemaLocation'] = 'http://dd.eionet.europa.eu/schemas/mmr-necs/NEC_PAMs.xsd';                                   
-            else $scope.instance.NEC_PAMs['@xmlns:noNamespaceSchemaLocation'] = instance.NEC_PAMs['@xmlns:noNamespaceSchemaLocation'];
+            if (!instance.NEC_PAMs['@xsi:noNamespaceSchemaLocation']) $scope.instance.NEC_PAMs['@xsi:noNamespaceSchemaLocation'] = 'http://dd.eionet.europa.eu/schemas/mmr-necs/NEC_PAMs.xsd';                                   
+            else $scope.instance.NEC_PAMs['@xsi:noNamespaceSchemaLocation'] = instance.NEC_PAMs['@xsi:noNamespaceSchemaLocation'];
 
             if (!instance.NEC_PAMs['@xml:lang']) $scope.instance.NEC_PAMs['@xml:lang'] = 'en';                                   
             else $scope.instance.NEC_PAMs['@xml:lang'] = instance.NEC_PAMs['@xml:lang'];
